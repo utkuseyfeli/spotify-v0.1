ecb44612c83125b2821f7f69b958f20e845cb3c8
Uğur Utku Seyfeli <utkuseyfeli@gmail.com>
2021-08-27 14:21:11 +0300

    all karma and jasmine packages removed

--- spotify-app/src/app/artist/artist.component.spec.ts
+++ spotify-app/src/app/artist/artist.component.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ArtistComponent } from './artist.component';
+import { ArtistComponent } from "./artist.component";
 
-describe('ArtistComponent', () => {
+describe("ArtistComponent", () => {
   let component: ArtistComponent;
   let fixture: ComponentFixture<ArtistComponent>;
 
@@ -10,7 +10,7 @@ describe('ArtistComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ ArtistComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +19,7 @@ describe('ArtistComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
